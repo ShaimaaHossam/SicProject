a6cbf299cb702e9284f9092791dfe75049557483
Shaimaa98 <40139825+Shaimaa98@users.noreply.github.com>
2020-11-19 19:07:44 +0200

    Addressing instructions

--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,15 @@ import java.util.*;
 
 public class Main {
 
+     Dictionary instructionSet = new Hashtable();
+
     public static void main(String[] args){
         ArrayList<String> prog = new ArrayList<>() ;
         int start=0;
         String startAddress = "";
         //Read file
         try{
-            File myObj=new File("in2.txt");
+            File myObj=new File("in.txt");
             Scanner myReader=new Scanner(myObj);
             while(myReader.hasNextLine()){
                 String data=myReader.nextLine();
@@ -33,22 +35,33 @@ public class Main {
             String[] line = s.split(" ");
             if(line[1].equals("Start") ){
                 start = Integer.parseInt(line[2],16);
-                String incHex = Integer.toHexString(start);
-                s = incHex + " " +s;
-                startAddress = incHex;
+                String addressHex = String.format("%04X", start);
+                s = addressHex + " " +s;
+                startAddress = addressHex;
                 prog.set(i,s);
                 //System.out.println(s);
+
             }
+            else if(line[1].equals("RESW")){
+
+                int n = Integer.parseInt(line[2])*3;
+                start+=n;
+                String addressHex = String.format("%04X", start);
+                s = addressHex + " " +s;
+                prog.set(i,s);
+
+            }
+
             else if(i==1){
                 //System.out.println(startAddress + " " +s);
                 prog.set(i,startAddress+" "+s);
             }
             else{
                 start+=3;
-                String incHex = Integer.toHexString(start);
-                s = incHex + " " +s;
+                String addressHex = String.format("%04X", start);
+                s = addressHex + " " +s;
                 prog.set(i,s);
-                //System.out.println(s);
+
             }
         }
         for(int i =0; i<prog.size(); i++){
@@ -57,5 +70,6 @@ public class Main {
 
 
     }
+
 
 }
