c5a5eb80e4cc2a4628a19489243b70efea633038
Shaimaa98 <40139825+Shaimaa98@users.noreply.github.com>
2020-11-21 23:57:05 +0200

    Code now converts addresses to hexadecimals accurately

--- src/pass1.java
+++ src/pass1.java
@@ -2,25 +2,27 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.math.BigInteger;
 
 public class pass1 {
 
     ArrayList<String> prog;
     ArrayList<String> symbols = new ArrayList<>();
-    int start=0;
+    BigInteger start ;
     String startAddress = "";
 
     public ArrayList<String> read_file()
     {
+        System.out.println("PASS 1 READ: \n");
         prog = new ArrayList<>() ;
         //Read file
         try{
 
-            File myObj=new File("in.txt");
+            File myObj=new File("in2.txt");
             Scanner myReader=new Scanner(myObj);
             while(myReader.hasNextLine()){
                 String data=myReader.nextLine();
-//                System.out.println(data);
+                System.out.println(data);
                 prog.add(data);
             }
             myReader.close();
@@ -31,64 +33,64 @@ public class pass1 {
             e.printStackTrace();
         }
 
-         return prog;
+        return prog;
     }
 
     public ArrayList<String> Generate_LOCCR(){
-//        System.out.println("\n\n\nGENERATE LOCATION COUNTER: \n");
+        System.out.println("\n\n\nGENERATE LOCATION COUNTER: \n");
         for(int i=0; i<prog.size(); i++ ){
-            String s = prog.get(i);
 
+            String s = prog.get(i);
             String[] line = s.split(" ");
             if(line[1].equals("Start") ){
-                start = Integer.parseInt(line[2]);
+                start = new BigInteger(line[2], 16);
                 String addressHex = String.format("%04X", start);
                 s = addressHex + " " +s;
                 startAddress = addressHex;
                 prog.set(i,s);
             }
             else if(line[1].equals("RESW") && Integer.parseInt(line[2]) >1){
-
-                int n = Integer.parseInt(line[2]);
+                int address = Integer.parseInt(line[2])*3;
+                String num = Integer.toHexString(address);
+                BigInteger n = new BigInteger(num, 16);
                 String addressHex = String.format("%04X", start);
                 s = addressHex + " " +s;
                 prog.set(i,s);
-                start = start + n*3;
+                start = start.add(n);
             }
 
             else if(i==1){
-                //System.out.println(startAddress + " " +s);
                 prog.set(i,startAddress+" "+s);
-                start+=3;
+                start = start.add(new BigInteger("3",16));
             }
             else{
 
                 String addressHex = String.format("%04X", start);
                 s = addressHex + " " +s;
                 prog.set(i,s);
-                start+=3;
+                start = start.add(new BigInteger("3", 16));
             }
         }
+
         for(int i =0; i<prog.size(); i++){
-//            System.out.println(prog.get(i));
+          System.out.println(prog.get(i));
         }
 
         return prog;
     }
 
     public ArrayList<String> Symbol_table(){
-//        System.out.println("\n\nSYMBOL TABLE:");
+        System.out.println("\n\nSYMBOL TABLE:\n");
         for(int i=1; i<prog.size(); i++){
             String instruction = prog.get(i);
             String[] line = instruction.split(" ");
             if(line.length > 3 ){
-                //symbols.add(line[1]);
                 symbols.add(line[0] + " "+line[1]);
             }
         }
 
         for(int i =0; i<symbols.size(); i++){
-//            System.out.println(symbols.get(i));
+           System.out.println(symbols.get(i));
         }
         return symbols;
     }
